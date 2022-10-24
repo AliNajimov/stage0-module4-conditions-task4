9b760c931a225c8b9d754569a1d7aadf0845ce69
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-24 18:51:42 +0500

    Update MaxNumberPrinter.java

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        int d = a > (b > c ? b : c) ? a : (b > c ? b : c);
+        int d = first > (second > third ? second : third) ? first : ((second > third) ? second : third);
         System.out.println(d);
     }
 }
